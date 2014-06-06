a03473848425146cbdc3a3f2f12619e4f72edfc0
Afonso <noreply>
2014-06-06 22:48:51 +0000

    auto-recovery

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -13,5 +13,34 @@ exports.Main = Component.specialize(/** @lends Main# */ {
         value: function Main() {
             this.super();
         }
+    },
+    
+    count: {value: 0},
+    array: {value: null},
+
+    templateDidLoad: {
+        value: function() {
+            var array = [];
+
+            for (var i = 0; i < this.total; i++) {
+                array[i] = i;
+            }
+            this.array = array;
+
+            this.needsDraw = true;
+        }
+    },
+
+    draw: {
+        value: function() {
+            var count = this.count++;
+
+            this.top = Math.sin(count / 10) * 10;
+            this.left = Math.cos(count / 10) * 10;
+            this.color = count % 255;
+            this.content = count % 100;
+
+            this.needsDraw = true;
+        }
     }
 });
