cbc5ab91b882ab1bd3849dea5f2f2be340c65778
Afonso <noreply>
2014-06-06 22:00:37 +0000

    Add component box

---
+++ ui/box.reel/box.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/box.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Box
+ * @extends Component
+ */
+exports.Box = Component.specialize(/** @lends Box# */ {
+    constructor: {
+        value: function Box() {
+            this.super();
+        }
+    }
+});
